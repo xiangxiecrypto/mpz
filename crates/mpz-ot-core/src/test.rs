94dcf7ab3bebeaaa89ec392797ba2fd6698076bd
Xiang Xie <xiexiangiscas@gmail.com>
2024-05-18 12:30:11 +0800

    clippy

--- crates/mpz-ot-core/src/test.rs
+++ crates/mpz-ot-core/src/test.rs
@@ -4,7 +4,7 @@ use mpz_core::Block;
 
 /// Asserts the correctness of correlated oblivious transfer.
 pub fn assert_cot(delta: Block, choices: &[bool], msgs: &[Block], received: &[Block]) {
-    assert!(choices.into_iter().zip(msgs.into_iter().zip(received)).all(
+    assert!(choices.iter().zip(msgs.iter().zip(received)).all(
         |(&choice, (&msg, &received))| {
             if choice {
                 received == msg ^ delta
@@ -17,7 +17,7 @@ pub fn assert_cot(delta: Block, choices: &[bool], msgs: &[Block], received: &[Bl
 
 /// Asserts the correctness of random oblivious transfer.
 pub fn assert_rot<T: Copy + PartialEq>(choices: &[bool], msgs: &[[T; 2]], received: &[T]) {
-    assert!(choices.into_iter().zip(msgs.into_iter().zip(received)).all(
+    assert!(choices.iter().zip(msgs.iter().zip(received)).all(
         |(&choice, (&msg, &received))| {
             if choice {
                 received == msg[1]
